6f9a75223d4ba219ce17de7e0bc77fc632b448c4
Galym <62488119+Galymjohn@users.noreply.github.com>
2021-02-25 17:06:01 -0600

    1

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,40 @@
+//function numberRoll {
+//  var randomNumber1 = Math.floor((Math.random() * 6) + 1); //integer between 1 amd 8. including both 1 and 8.
+//}
+// var arrayImages = [
+//   "C:/Users/johnn/Documents/GitHub/diceeGame/Dicee Challenge - Starting Files/images/dice1.png",
+//   "C:/Users/johnn/Documents/GitHub/diceeGame/Dicee Challenge - Starting Files/images/dice2.png",
+//   "C:/Users/johnn/Documents/GitHub/diceeGame/Dicee Challenge - Starting Files/images/dice3.png",
+//   "C:/Users/johnn/Documents/GitHub/diceeGame/Dicee Challenge - Starting Files/images/dice3.png",
+//   "C:/Users/johnn/Documents/GitHub/diceeGame/Dicee Challenge - Starting Files/images/dice5.png",
+//   "C:/Users/johnn/Documents/GitHub/diceeGame/Dicee Challenge - Starting Files/images/dice6.png"
+// ];
+// var arrayImages = [
+//   "\images\dice1.png",
+//   "\images\dice2.png",
+//   "\images\dice3.png",
+//   "\images\dice3.png",
+//   "\images\dice5.png",
+//   "\images\dice6.png"
+// ];
+//roll 1
+var randomNumber1 = Math.floor((Math.random() * 6) + 1); //integer between 1 amd 8. including both 1 and 8.
+var diceRoll1 = "images/dice" + randomNumber1 + ".png";
+document.images[0].src = diceRollOne;
+
+//rol 2
+// var randomNumber2 = Math.floor((Math.random() * 6) + 1); //integer between 1 amd 8. including both 1 and 8.
+// var diceRoll2 = "images/dice" + randomNumber2 + ".png";
+// document.images[1].src = diceRollOne;
+// var randomNumber2 = Math.floor((Math.random() * 6) + 1); //integer between 1 amd 8. including both 1 and 8.
+// //}
+// function rollOne() {
+//   diceRollOne = arrayImages[randomNumber1];
+//   document.images[0].setAttribute("src",diceRoll1); // in setAttribute(source,change to)
+//   //console.log(diceRollOne);
+// }
+// function rollTwo() {
+//   diceRolltwo = arrayImages[randomNumber2];
+//   document.images[1].setAttribute("src",diceRoll2);  //
+//   //console.log(diceRollOne)
+// }
